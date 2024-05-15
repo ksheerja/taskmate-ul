85c5e7b4fbe6aa80eceebd96c86a47fbcfe1146a
Ksheerja <ksheerja567@gmail.com>
2024-05-15 21:32:06 +1000

    Railway Project2

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -100,6 +100,7 @@ DATABASES = {
    }
 }
 
+postgresql://postgres:bVpewBseBPTeeVqQKNVCXExAvLrDkueu@roundhouse.proxy.rlwy.net:37798/railway
 
 
 # Password validation
@@ -138,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
